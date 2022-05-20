9f4227d7e0b6eee14bbcacf05c57ab3e3c1c15fc
Adar Schwarzbach <adar.schwarzbach@duke.edu>
2022-05-20 12:44:14 -0700

    created ranbow styler

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-const container = document.getElementById("container");
+
+
+
 
 
 function createGrid(perSide){
+    const container = document.getElementById("container");
     for(let x = 0; x<(perSide*perSide); x++){
         const percent = 100/perSide;
         let div = document.createElement("div");
@@ -16,12 +19,42 @@ function createGrid(perSide){
 }
 
 createGrid(50);
+let mode = 'rainbow';
 
+
+function reset(){
+    createGrid(16);
+    let mode = 'black';
+
+}
+
+
+// global to ensure user intends to draw (mousedown)
 let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true);
 document.body.onmouseup = () => (mouseDown = false);
 
 function changeColor(e){
     if(e.type== 'mouseover' && !mouseDown) return;
-    e.target.style.backgroundColor = "red";
+    switch(mode){
+        case 'black':
+            e.target.style.backgroundColor = "black";
+            break;
+        case 'rainbow':
+            const r = createRndInteger(0,256);
+            const g = createRndInteger(0,256);
+            const b = createRndInteger(0,256);
+            e.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+            break;
+        case 'eraser':
+            e.target.style.backgroundColor = "white";
+            break;
+        case 'choose':
+            break;
+    }
+}
+
+// Helper function to create random int [min,max]
+function createRndInteger(min, max){
+    return Math.floor(Math.random()*(max-min+1)) + min;
 }

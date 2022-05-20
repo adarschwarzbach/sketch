3f217b1b4295cde0f7aa06dcd97b2e0b604415e9
Adar Schwarzbach <adar.schwarzbach@duke.edu>
2022-05-19 22:01:21 -0700

    etch drawing mechanics completed

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 const container = document.getElementById("container");
+
+
+function sixteen(){
+    for(let x = 0; x<16; x++){
+        let div = document.createElement("div");
+        div.classList.add("sub");
+        div.addEventListener("mouseover",  changeColor);
+        div.addEventListener("mousedown",  changeColor);
+        console.log("hi")
+        container.appendChild(div);
+    }
+}
+
+sixteen()
+
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+function changeColor(e){
+    if(e.type== 'mouseover' && !mouseDown) return;
+    e.target.style.backgroundColor = "red";
+}
